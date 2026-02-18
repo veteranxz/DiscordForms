d83a4eff25b881be1a6f38b7e41d4b161f1c3370
veteranxz <veteranxz132@gmail.com>
2026-02-18 23:03:43 +0200

    Update discord.js

--- js/discord.js
+++ js/discord.js
@@ -103,7 +103,7 @@ function createDiscordEmbed(formData, imagesLength) {
       embed.fields.push({
         name: fieldName,
         value: displayValue,
-        inline: false,
+        inline: true,
       });
     }
   });
